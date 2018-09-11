1a28e1cfe887c13e501b51dfeaee8dc4b54936be
Dedrik <43129281+Dedrik@users.noreply.github.com>
2018-09-11 09:28:26 +0200

    Add files via upload

---
+++ Calculette1.java
@@ -0,0 +1,13 @@
+package com.cours.dolcic.calculette1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Calculette1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculette1);
+    }
+}
